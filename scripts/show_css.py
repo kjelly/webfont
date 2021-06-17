952abf5f735cc92e26351df07713d82bc39fe33a
Kuo-tung Kao <kjellytw@gmail.com>
2021-06-17 23:22:42 +0800

    Search from local repo

--- scripts/show_css.py
+++ scripts/show_css.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import os
+import re
+import shutil
+from subprocess import check_output
 from subprocess import check_output
 
 
 def main():
-    url = sys.argv[1]
-    download_url = '%s?raw=true' % url
-    os.system('wget "%s" -O /tmp/a.ttf -o /dev/null' % download_url)
+    font_dir_path = sys.argv[1]
+    font_path = check_output("find %s -name '*Nerd Font Complete Mono*f' -type f|grep Mono|grep Regu|fzf" % font_dir_path, shell=True).decode('utf-8').strip()
+    shutil.copy(font_path, '/tmp/a.ttf')
     font_name = check_output('otfinfo --info /tmp/a.ttf|head -n 1|grep Family', shell=True).decode('utf-8').strip().split(':')[-1].strip()
-    path = url.replace('https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts', '').strip()
+    url = re.sub(r'.*patched-fonts/', '', font_path)
+    font_dir_path = font_dir_path.replace('https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts', '').strip()
 
     output = '''
 @font-face {
@@ -17,7 +21,7 @@ def main():
   font-weight: normal;
   src: url("https://cdn.jsdelivr.net/gh/ryanoasis/nerd-fonts@bc4416e1/patched-fonts/%s");
 }
-    ''' % (font_name, path)
+    ''' % (font_name, url)
     print(output)
 
 
